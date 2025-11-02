a33668c5be7da094c8b98521c4656ac9a20b379f
ksvvr <97872880+ksvvr@users.noreply.github.com>
2025-11-02 23:09:05 +0530

    github

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/BillTheBrick',
   plugins: [react(),tailwindcss(),
     VitePWA({
           registerType: 'autoUpdate', // Or 'prompt' for manual update
